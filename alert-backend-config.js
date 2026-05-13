35e2dc3a0fac1b752cbe5c43198ac5b04dcfca03
Marcel Konrad <konradma@hs-pforzheim.de>
2026-05-13 18:36:46 +0200

    Add alert backend URL config

---
+++ alert-backend-config.js
@@ -0,0 +1,3 @@
+// Nach dem Backend-Deployment hier die Backend-URL eintragen, z. B.
+// window.TANKZEIT_ALERT_BACKEND_URL = "https://tankzeit-alerts.example.com";
+window.TANKZEIT_ALERT_BACKEND_URL = window.TANKZEIT_ALERT_BACKEND_URL || "";
